9ecf587c77fb6208ade0b8b88732af344cfad781
hanb613 <60910288+hanb613@users.noreply.github.com>
2022-07-07 12:21:03 +0900

    22.07.07 | 에라토스테네스의 체

---
+++ BOJ/BOJ_2960번/2960번, 에라토스테네스의 체.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <math.h>
+#include <queue>
+#include <tuple>
+using namespace std;
+#pragma warning(disable:4996)
+
+typedef long long int lli;
+
+int n, k, cnt;
+int arr[1001];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> k;
+
+	for (int i = 2; i <= n; i++) {
+		arr[i] = i;
+	}
+
+	for (int i = 2; i <= n; i++) {
+		for (int j = i, m = 1; j <= n; m++, j = i * m) {
+			if (arr[j] == 0) continue;
+
+			cnt++; arr[j] = 0;
+			if (cnt == k) {
+				cout << j;
+				return 0;
+			}
+		}
+	}
+}
