6262c6107f8c16c08c58091d24777cf138f9f696
hanb613 <yeoongs2@gmail.com>
2022-11-06 17:58:05 +0900

    22.11.06 | 경로 찾기

---
+++ BOJ/BOJ_11403번/11403번, 경로 찾기.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <math.h>
+#include <queue>
+#include <tuple>
+using namespace std;
+#pragma warning(disable:4996)
+
+typedef long long int lli;
+
+int n;
+int graph[101][101];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> graph[i][j];
+		}
+	}
+
+	for (int k = 0; k < n; k++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (graph[i][k] && graph[k][j]) {
+					graph[i][j] = 1;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << graph[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
