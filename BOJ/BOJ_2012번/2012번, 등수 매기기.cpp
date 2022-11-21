4b11eef066bd9c815116ea905b5e7c5b23a522ba
hanb613 <yeoongs2@gmail.com>
2022-11-21 17:28:28 +0900

    22.11.21 | 등수 매기기

---
+++ BOJ/BOJ_2012번/2012번, 등수 매기기.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <math.h>
+#include <queue>
+#include <tuple>
+using namespace std;
+#pragma warning(disable:4996)
+
+typedef long long int lli;
+
+int n;
+int arr[500001];
+lli ans;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	sort(arr, arr + n);
+
+	for (int i = 0; i < n; i++) {
+		ans += abs((i + 1) - arr[i]);
+	}
+
+	cout << ans;
+}
---
+++ BOJ/BOJ_2012번/README.md
@@ -0,0 +1,6 @@
+## BOJ 2012번 '등수 매기기' 문제 
+
+* _그리디 알고리즘_
+* _정렬_
+
+* https://www.acmicpc.net/problem/2012
