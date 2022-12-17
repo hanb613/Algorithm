f511ed7c9ab8aa55a54ec19e76fd76f4a90211f8
hanb613 <yeoongs2@gmail.com>
2022-12-17 18:07:10 +0900

    22.12.17 | 위장

---
+++ Programmers/PRO_위장.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+	int answer = 1;
+
+	map<string, int> m;
+	for (int i = 0; i < clothes.size(); i++) {
+		m[clothes[i][1]]++;
+	}
+
+	map<string, int>::iterator it;
+	for (it = m.begin(); it != m.end(); it++) {
+		answer *= (it->second + 1);
+	}
+
+	return answer - 1;
+}
