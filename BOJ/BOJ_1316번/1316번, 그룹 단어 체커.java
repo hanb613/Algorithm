9c1d8028fe076a3d90e88db005c475470cc99b8f
hanb613 <yeoongs2@gmail.com>
2022-12-23 21:08:50 +0900

    22.12.23 | 그룹 단어 체커

---
+++ BOJ/BOJ_1316번/1316번, 그룹 단어 체커.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int cnt = num;
+
+        for(int i=0; i<num; i++){
+            boolean check[] = new boolean[26];
+            String str = sc.next();
+
+            for(int j=0; j<str.length()-1; j++){
+                if(str.charAt(j) != str.charAt(j+1)){
+                    if(check[str.charAt(j+1) -97] == true){
+                        cnt--;
+                        break;
+                    }
+                }
+                check[str.charAt(j)-97]=true;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
---
+++ BOJ/BOJ_1316번/README.md
@@ -0,0 +1,6 @@
+## BOJ 1316번 '그룹 단어 체커' 문제 
+
+* _구현_
+* _문자열_
+
+* https://www.acmicpc.net/problem/1316
