61ee33d005dca1882c6d4b298f64d877a256709e
hanb613 <yeoongs2@gmail.com>
2022-12-23 21:13:01 +0900

    22.12.23 | 그룹 단어 체커

--- BOJ/BOJ_1316번/1316번, 그룹 단어 체커.java
+++ BOJ/BOJ_1316번/1316번, 그룹 단어 체커.java
@@ -6,18 +6,18 @@ public class Main {
         int num = sc.nextInt();
         int cnt = num;
 
-        for(int i=0; i<num; i++){
+        for(int i = 0; i < num; i++){
             boolean check[] = new boolean[26];
             String str = sc.next();
 
-            for(int j=0; j<str.length()-1; j++){
-                if(str.charAt(j) != str.charAt(j+1)){
-                    if(check[str.charAt(j+1) -97] == true){
+            for(int j = 0; j < str.length() - 1; j++){
+                if(str.charAt(j) != str.charAt(j + 1)){
+                    if(check[str.charAt(j + 1) - 97] == true){
                         cnt--;
                         break;
                     }
                 }
-                check[str.charAt(j)-97]=true;
+                check[str.charAt(j) - 97]=true;
             }
         }
         System.out.println(cnt);
