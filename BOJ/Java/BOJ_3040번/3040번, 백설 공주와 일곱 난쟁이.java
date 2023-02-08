9c201e8dd635914ff98291cc70e47b3e887b5fdb
유영서 <you2882@naver.com>
2023-02-08 19:59:43 +0900

    23.02.08 | 백설 공주와 일곱 난쟁이

---
+++ BOJ/Java/BOJ_3040번/3040번, 백설 공주와 일곱 난쟁이.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int[] arr;
+	static boolean[] visit;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int total =0;
+        arr = new int[9];
+        for(int i=0; i<9; i++) {
+        	arr[i] = Integer.parseInt(br.readLine());
+        	total+=arr[i];
+        }
+
+        for(int i=0; i<9; i++) {
+        	for(int j=i+1; j<9; j++) {
+        		if(total - arr[i] -arr[j] ==100) {
+        			for(int k=0; k<9; k++) {
+        	        	if(i==k || j==k) continue;
+        	        	System.out.println(arr[k]);
+        	        }
+        		}
+        	}
+        }
+    }
+}
---
+++ BOJ/Java/BOJ_3040번/README.md
@@ -0,0 +1,5 @@
+## BOJ 3040번 '백설 공주와 일곱 난쟁이' 문제 
+
+* _브루트포스 알고리즘_
+
+* https://www.acmicpc.net/problem/3040
