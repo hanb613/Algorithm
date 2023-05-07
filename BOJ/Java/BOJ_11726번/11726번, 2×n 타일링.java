58770d06377574efa7d5c364e74fe4e73ad5bf24
유영서 <you2882@naver.com>
2023-05-07 12:37:48 +0900

    23.05.07 | 2×n 타일링

---
+++ BOJ/Java/BOJ_11726번/11726번, 2×n 타일링.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	static int n;
+	static int[] arr;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		n = Integer.parseInt(br.readLine());
+		arr = new int[1001];
+		
+		arr[1]=1;
+		arr[2]=2;
+		
+		for(int i=3; i<=1000; i++) {
+			arr[i] = (arr[i-1] + arr[i-2]) %10007;
+		}
+		
+		System.out.println(arr[n]);
+	}
+}
