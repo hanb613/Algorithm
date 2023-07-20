ba09905fe6816553c9589da3183f11704e834469
Yeong-seo Yoo <yeoongs2@gmail.com>
2023-07-21 08:41:21 +0900

    23.07.21 | 차이를 최대로

--- BOJ/Java/BOJ_10819번/10819번, 차이를 최대로.java
+++ BOJ/Java/BOJ_10819번/10819번, 차이를 최대로.java
@@ -1,47 +1,53 @@
-import java.util.*;
-import java.io.*;
-public class Main{
-    public static int n, result;
-    public static int[] arr, ans;
-    public static boolean[] visit;
-    
-   
-    public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-    	n = Integer.parseInt(br.readLine());
-    	
-        arr = new int[n];
-        ans = new int[n];
-        visit = new boolean[n];
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        dfs(0);
-        
-        System.out.println(result);
-    } 
-    
-    private static void dfs(int cnt){
-        if(cnt == n) {
-            int sum = 0;
-            for(int i = 0; i < n-1; i++) {
-                sum += Math.abs(ans[i] - ans[i+1]);
-            }
-            result = Math.max(result, sum);
-            return;
-        }
-        
-        for(int i = 0; i < n; i++) {
-            if(!visit[i]){
-                ans[cnt] = arr[i];
-                visit[i] = true;
-                dfs(cnt + 1);
-                visit[i] = false;
-            }
-        }
-    }
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int n, result;
+	static int[] arr, num;
+	static boolean[] visited;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n];
+		num = new int[n];
+		visited = new boolean[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		Solution(0);
+		
+		System.out.println(result);
+	}
+	
+	private static void Solution(int k) {
+		if(k==n) {
+			int sum=0;
+			for(int i=0; i<n-1; i++) {
+				sum += Math.abs(num[i]-num[i+1]);
+			}
+			result = Math.max(result, sum);
+			return;
+		}
+		
+		for(int i=0; i<n; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				num[k]=arr[i];
+				Solution(k+1);
+				visited[i] = false;
+			}
+		}
+		
+	}
 }
