eced15579c0c5e77ddace0b074d75f84f8ac6231
유영서 <you2882@naver.com>
2023-09-21 21:14:57 +0900

    23.09.21 | 숫자의 표현

---
+++ Programmers/Java/Lv2_숫자의 표현.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=1; i<=n; i++){
+            int sum=0;
+            for(int j=i; j<=n; j++){
+                if(sum+j>n) break;
+                if(sum+j==n) {
+                    answer++; break;
+                }
+                
+                sum+=j;
+            }
+        }
+        
+        return answer;
+    }
+}
