4767eac5f46ceee60ef22ad90ae33f526b88427c
유영서 <you2882@naver.com>
2023-10-11 20:41:42 +0900

    23.10.11 | 암호코드

---
+++ BOJ/Java/BOJ_2011번/2011번, 암호코드.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static String str;
+	static long[] dp;
+	static final int MOD = 1000000;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				
+		str = br.readLine();
+		
+		long[] dp = new long[str.length()+1];
+		
+		if(str.charAt(0) == '0') {
+			System.out.println(0);
+		} else {
+			dp[0]=dp[1]=1;
+			
+			for(int i=2; i<=str.length(); i++) {
+				char now = str.charAt(i-1);
+				char prev = str.charAt(i-2);
+				if(now == '0') { // 현재 위치가 0이면 ? 앞에 1이나 2가 와야됨
+					if(prev=='1' || prev=='2') {
+						dp[i] = dp[i-2] % MOD;
+					} else break;
+				} else {
+					int result = Integer.parseInt(str.substring(i-2, i));
+
+					if(result>=27 || result<=9) {
+						dp[i] = dp[i-1] % MOD;
+					}else {
+						dp[i] = (dp[i-1]+dp[i-2]) % MOD;
+					}
+				}
+			}
+			
+			System.out.println(dp[str.length()] % MOD);
+		}
+	
+	}
+}
---
+++ BOJ/Java/BOJ_2011번/README.md
@@ -0,0 +1,5 @@
+## BOJ 2011번 '암호코드' 문제 
+
+* _다이나믹 프로그래밍_
+
+* https://www.acmicpc.net/problem/2011
