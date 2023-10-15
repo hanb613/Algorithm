a7d12351def2d480837f0a849f317e3545c85035
유영서 <you2882@naver.com>
2023-10-15 12:16:23 +0900

    23.10.15 | IQ Test

---
+++ BOJ/Java/BOJ_1111번/1111번, IQ Test.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static int n, result;
+	static int[] arr;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n];
+
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		if(n==1) {
+			System.out.println("A");
+		} else if(n==2) {
+			if(arr[0]==arr[1]) {
+				System.out.println(arr[1]);
+			} else {
+				System.out.println("A");
+			}
+		} else {
+			int a, b;
+			
+			if(arr[1]==arr[0]) {
+				a = 1; 
+				b = 0;
+			} else {
+				a = (arr[2]-arr[1]) / (arr[1]-arr[0]);
+				b = arr[1] - (arr[0]*a);
+			}
+
+			for(int i=0; i<n-1; i++) {
+				if(arr[i+1] != (arr[i]*a)+b) {
+					System.out.println("B");
+					return;
+				}
+			}
+			
+			System.out.println((arr[n-1]*a)+b);
+		}
+	}
+	
+}
---
+++ BOJ/Java/BOJ_1111번/README.md
@@ -0,0 +1,8 @@
+## BOJ 1111번 'IQ Test' 문제 
+
+* _수학_
+* _구현_
+* _브루트포스 알고리즘_
+* _많은 조건 분기_
+
+* https://www.acmicpc.net/problem/1111
